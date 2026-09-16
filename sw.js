dbe842de5f674d74a423d14420da26ab8796d1df
LeonOAO <kkwan812@gmail.com>
2026-09-16 14:46:12 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 "use strict";
 
-// OwO Web 2.4.9: robust Scramjet frame navigation and login-session persistence.
+// OwO Web 2.5.0: robust Scramjet frame navigation and login-session persistence.
 
 importScripts("./Scramjet/scramjet.all.js");
 
